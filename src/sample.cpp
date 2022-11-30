052b074def59c00721a0033f0cd3396790e6e916
liuyu <queeniiee02@gmail.com>
2022-11-30 16:04:15 +0800

    add Ball class and simple usage

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,10 +1,14 @@
 #include "graphics.h"
 
-int main(int argc, char*argv[]){
-	int x = 120, y = 120, dx = 5, dy = 1, ddy = 2, r = 100;
-	initwindow(800, 640);	//window size
-	while (1){
-		if(kbhit()) break;
+class Ball {
+public:
+    Ball() {
+        r = 50, dx = 5, dy= 1, x = 0, y = 0;
+        color = GREEN;
+        ddy = 2;
+    }
+    int r, dx, dy, ddy, x, y, color;
+    void motion () {
 		dy += ddy;
 		x += dx; 
         if (x < r) {
@@ -13,7 +17,6 @@ int main(int argc, char*argv[]){
         if (x >= getmaxx() - r) {
             dx = -5;
         }  
-
         if (y < r && dy < 0) {
             dy = -dy;
         }
@@ -23,74 +26,23 @@ int main(int argc, char*argv[]){
         else {
             y += dy;
         }
+    }
+};
+
+int main(int argc, char*argv[]){
+	//int x = 120, y = 120, dx = 5, dy = 1, ddy = 2, r = 100;
+    Ball b; // must be outside the while function
+	initwindow(800, 640);	//window size
+	while (1){
+		if(kbhit()) break;
 		cleardevice();
 		setcolor(GREEN);
 		setfillstyle(SOLID_FILL, BLUE);
-		fillellipse(x, y, r, r);
+		fillellipse(b.x, b.y, b.r, b.r);
+        b.motion();
 		refresh();
 		delay(10);
 	}
 	closegraph();
 }
 
-/*
-
-#include "SDL2/SDL.h" 
-
-int main(int argc, char *argv[])
-{
-    SDL_Window *window = NULL;
-    SDL_Renderer *renderer = NULL;
-    SDL_Surface *surface = NULL;
-    SDL_Texture *texture = NULL;
-    SDL_Event event;
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
-        return 3;
-    }
-
-    window = SDL_CreateWindow("Hello World", 50, 50, 1280, 720, SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
-    if (!window) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window: %s", SDL_GetError());
-        return 3;
-    }
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create renderer: %s", SDL_GetError());
-        return 3;
-    }
-
-    surface = SDL_LoadBMP("./img/sample.bmp");
-    if (!surface) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
-        return 3;
-    }
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (!texture) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
-        return 3;
-    }
-    SDL_FreeSurface(surface);
-
-    while (1) {
-        SDL_PollEvent(&event);
-        if (event.type == SDL_QUIT) {
-            break;
-        }
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
-
-    return 0;
-}
-*/
