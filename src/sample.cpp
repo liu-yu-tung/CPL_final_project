bc00e4f5584d244d1502e58d2e411e56cd06c171
liuyu <queeniiee02@gmail.com>
2022-12-01 06:34:47 +0800

    seperate .cpp files

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,39 +1,5 @@
 #include "ball.h"
 
-Ball::Ball() {
-    r = 50, dx = 5, dy= 1, x = 0, y = 0, v = 5;
-    color = GREEN;
-    ddy = 1;
-}
-Ball::Ball (int _r, int _x, int _y, int _v, int _color) {
-    r = _r, x = _x, y = _y, v = _v, color = _color;
-    dy = 1;
-    ddy = 1;
-}
-void Ball::randomGenerator () {
-    r = std::uniform_int_distribution<>(25,125)(eng);
-    x = std::uniform_int_distribution<>(0, getmaxx())(eng);
-    y = std::uniform_int_distribution<>(0, getmaxy())(eng);
-    v = std::uniform_int_distribution<>(1, 30)(eng);
-    dy = std::uniform_int_distribution<>(1, 5)(eng);
-    color = std::uniform_int_distribution<>(0, 15)(eng);
-    ddy = 1;
-}
-void Ball::motion () {
-    dy += ddy;
-    x += dx; 
-    if (x < r) dx = v;
-    else if (x >= getmaxx() - r) dx = -v;
-    if (y < r && dy < 0) dy = -dy;
-    else if (y >= getmaxy() - r && dy > 0) dy = - dy;
-    else y += dy;
-}
-std::mt19937 Ball::seeded_engine() { 
-    std::random_device r;
-    std::seed_seq seed{r(), r(), r(), r(), r(), r(), r(), r()};
-    return std::mt19937(seed);
-}
-
 int main(int argc, char*argv[]){
     /*==============
         create object outside of the while loop
