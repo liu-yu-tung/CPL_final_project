24e40d48b250d197efcd4968551cb32fb9cc559b
liuyu <queeniiee02@gmail.com>
2022-11-30 16:13:43 +0800

    add new Ball constructor

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,4 +1,6 @@
 #include "graphics.h"
+#include <iostream>
+#include <time.h>
 
 class Ball {
 public:
@@ -6,6 +8,11 @@ public:
         r = 50, dx = 5, dy= 1, x = 0, y = 0;
         color = GREEN;
         ddy = 2;
+    }
+    Ball (int _r, int _x, int _y, int _dx, int _color) {
+       r = _r, x = _x, y = _y, dx = _dx, color = _color;
+       dy = 1;
+       ddy = 2;
     }
     int r, dx, dy, ddy, x, y, color;
     void motion () {
@@ -31,15 +38,20 @@ public:
 
 int main(int argc, char*argv[]){
 	//int x = 120, y = 120, dx = 5, dy = 1, ddy = 2, r = 100;
+    srand(time(NULL));
     Ball b; // must be outside the while function
+    Ball c(100, 0, 100, 3, YELLOW);
 	initwindow(800, 640);	//window size
 	while (1){
 		if(kbhit()) break;
 		cleardevice();
 		setcolor(GREEN);
-		setfillstyle(SOLID_FILL, BLUE);
+		setfillstyle(SOLID_FILL, b.color);
 		fillellipse(b.x, b.y, b.r, b.r);
         b.motion();
+		setfillstyle(SOLID_FILL, c.color);
+		fillellipse(c.x, c.y, c.r, c.r);
+        c.motion();
 		refresh();
 		delay(10);
 	}
