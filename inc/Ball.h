fd03c004331e73ecf6a45b49c9d3db1c68acf0cb
liuyu <queeniiee02@gmail.com>
2022-12-01 06:29:25 +0800

    seperate files

---
+++ inc/Ball.h
@@ -0,0 +1,20 @@
+#ifndef BALL_H
+#define BALL_H
+
+#include "graphics.h"
+#include <iostream>
+#include <random>
+
+class Ball {
+public:
+    Ball();
+    Ball (int _r, int _x, int _y, int _v, int _color);
+    void randomGenerator ();
+    void motion ();
+    int r, dx, dy, ddy, x, y, color, v;
+private:
+    std::mt19937 seeded_engine();
+    std::mt19937 eng = seeded_engine();
+};
+
+#endif
